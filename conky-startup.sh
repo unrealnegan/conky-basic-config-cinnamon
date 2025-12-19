3b71c79b16f0fd87ef38b7c14bd8ca15c4533fa7
unrealnegan <138445041+unrealnegan@users.noreply.github.com>
2025-12-19 02:46:37 +0100

    Update conky-startup.sh

--- conky-startup.sh
+++ conky-startup.sh
@@ -3,7 +3,7 @@
 if [ "$DESKTOP_SESSION" = "cinnamon" ]; then 
    sleep 0s
    killall conky
-   cd "$HOME/.conky/Basic"
-   conky -c "$HOME/.conky/Basic/Basic" &
+   cd "$HOME/.conky/basic"
+   conky -c "$HOME/.conky/basic/basic" &
    exit 0
 fi
